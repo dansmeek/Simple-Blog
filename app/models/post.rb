ead4c63eccc4bbce2822ae5f34b7b518f47656e5
Adam Lipschultz <adamdlipsch@gmail.com>
2012-06-09 00:48:23 -0700

    files added

---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ActiveRecord::Base
+  attr_accessible :text, :title
+  has_many :comments
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '5932bb89f539ee7a903074d7163c83ca4c591888b491725f7b41ec1db0e2807819c884ba984edaa434da290830ed9059a64f7b90d0c16e71ad1f3ce243955e74'
